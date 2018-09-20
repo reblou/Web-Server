3359dec903866073ed33f6fd8d7100290b447ebd
Rebecca <rebeayto@gmail.com>
2018-09-20 16:25:41 +0100

    modifying global variables

--- threads_practise.c
+++ threads_practise.c
@@ -2,6 +2,9 @@
 #include <pthread.h>
 
 void *thread_code(void *arg);
+void *increment(void *arg);
+
+int val = 0;
 
 int main()
 {
@@ -9,7 +12,7 @@ int main()
 
     int i;
     for(i =0; i<10; i++) {
-        pthread_create(&threads[i], NULL, &thread_code, (void *) &i);
+        pthread_create(&threads[i], NULL, &increment, (void *) &i);
     }
 
     void *retval = NULL;
@@ -18,9 +21,20 @@ int main()
         pthread_join(threads[i], &retval);
         printf("joined: %d\n", *((int *) retval));
     }
+
+    printf("final val: %d\n", val);
 }
 
 void *thread_code(void *arg) {
     printf("thread %d\n", *((int *) arg));
+    return arg;
+}
+
+void *increment(void *arg) {
+    int *id = (int *) arg;
+
+    val++;
+
+    printf("thread: %d, val: %d\n", *id, val);
     return arg;
 }
